822c637274c3a53d90538f9578ed2f4e917249c7
Muhammadjon <muhmmadjon141998@gmail.com>
2022-11-08 11:37:24 +0500

    removed BrowerRouter from App.js and put that into main index.js and removed previous context from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import 'react-toastify/dist/ReactToastify.css';
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { AuthContext } from './context';
 
@@ -10,28 +10,15 @@ import Messages from './pages/messages/Messages';
 const existedToken = localStorage.getItem('token') || '';
 
 function App() {
-	const [isAuth, setIsAuth] = useState(!!existedToken);
-	const [user, setUser] = useState(null);
-	const [token, setToken] = useState(existedToken);
+	const { jwt, user, isAuth } = useContext(AuthContext);
 	return (
 		<>
 			<ToastContainer />
-			<AuthContext.Provider value={{
-				isAuth,
-				setIsAuth,
-				user,
-				setUser,
-				token,
-				setToken
-			}}>
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/messages/:username" element={<Messages />} />
-						{/* <Route path="*" element={<Navigate replace to={isAuth ? '/users' : '/login'} />} /> */}
-					</Routes>
-				</BrowserRouter>
-			</AuthContext.Provider>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/messages/:username" element={<Messages />} />
+				{/* <Route path="*" element={<Navigate replace to={isAuth ? '/users' : '/login'} />} /> */}
+			</Routes>
 		</>
 	);
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import './assets/style/index.scss';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import AuthProvider from './context';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<AuthProvider>
-		<App />
-	</AuthProvider>
+	<BrowserRouter>
+		<AuthProvider>
+			<App />
+		</AuthProvider>
+	</BrowserRouter>
 );
