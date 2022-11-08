34192ad35ac9fada49bf21dc242240d30a55c572
Muhammadjon <muhmmadjon141998@gmail.com>
2022-11-08 15:57:14 +0500

    removed not useful code from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,14 @@ import Messages from './pages/messages/Messages';
 function App() {
 	const { jwt, user, isAuth } = useContext(AuthContext);
 	return (
-		<>
+		<div className="wrapper">
 			<ToastContainer />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/messages/:username" element={<Messages />} />
 				{/* <Route path="*" element={<Navigate replace to={isAuth ? '/users' : '/login'} />} /> */}
 			</Routes>
-		</>
+		</div>
 	);
 }
 export default App;
