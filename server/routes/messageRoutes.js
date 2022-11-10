0509ec34de929693d6aae7172cee02270168390a
Muhammadjon <muhmmadjon141998@gmail.com>
2022-11-10 12:52:27 +0500

    improved non-correct routes

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -3,38 +3,10 @@ const messageController = require("../controllers/messageController")
 
 
 
-
-const roleMiddleware = (...roles) => {
-
-  return (req, res, next) => {
-
-    let roles2;
-
-    if (typeof roles === "string") {
-      roles2 = [roles]
-    } else {
-      roles2 =roles
-    }
-
-    
-
-    if (roles2.includes(req.user.role)) {
-      next(new Error("sdfsdf", 403))
-    } else {
-      next()
-    }
-  }
-  
-  
-}
-
-
-
-
 router
-.get("/recipients/:s", roleMiddleware("Admin", "Courier"), messageController.getAllRecipients)
-.get("/:name", roleMiddleware("Courier"), messageController.getAll)
-.post("/", messageController.create)
+	.get("/recipients/:s", messageController.getAllRecipients)
+	.get("/:name", messageController.getAll)
+	.post("/", messageController.create)
 
 
 module.exports = router
